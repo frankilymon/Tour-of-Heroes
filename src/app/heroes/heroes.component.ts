dee5f91e32352f9a20fe3560b6e1f283b5636d46
franklin <frank_ukaegbu@yahoo.com>
2020-12-18 06:29:42 -0500

    successfully updated

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,35 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../model/hero';
 import { HeroService } from '../model/hero.service';
+import { MessageService } from '../model/message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
+  selectedHero: Hero;
+
   heroes: Hero[];
-   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
-getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-}
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
-
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
